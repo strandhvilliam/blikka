f851771504f98800c12151d630737a5da61b1177
Villiam Strandh <villiam.strandh@outlook.com>
2026-02-09 23:16:31 +0100

    test log

--- apps/web-v2/src/app/(public)/[locale]/page.tsx
+++ apps/web-v2/src/app/(public)/[locale]/page.tsx
@@ -1,8 +1,14 @@
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  console.log("ENV", process.env.NODE_ENV)
+  console.log("NEXT_PUBLIC_ENV", process.env.NEXT_PUBLIC_ENV)
+  console.log("AUTH_URL", process.env.BETTER_AUTH_URL)
+  console.log("BLIKKA_PRODUCTION_URL", process.env.BLIKKA_PRODUCTION_URL)
+
   return (
     <div>
       <h1>Home</h1>
     </div>
   )
-}
+} 
