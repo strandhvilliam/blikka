87d6562fdd5c8c4adefb06d6f3cf81149cef1cf8
Villiam Strandh <villiam.strandh@outlook.com>
2026-06-14 17:23:46 +0200

    remove deprecated code

--- tasks/upload-finalizer/src/deprecated/utils.ts
+++
@@ -1,8 +0,0 @@
-import { Resource as SSTResource } from 'sst'
-
-export const getEnvironment = (): 'prod' | 'dev' | 'staging' => {
-  const stage = SSTResource.App.stage
-  if (stage === 'production') return 'prod'
-  if (stage === 'dev' || stage === 'development') return 'dev'
-  return 'staging'
-}
